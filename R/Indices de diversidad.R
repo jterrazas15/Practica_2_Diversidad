1d078d03bec3be280dacfa822d10a7681dc5889c
César Terrazas <jterrazas15@alumnos.uaq.mx>
2026-03-16 21:21:31 -0600

    Indices de diversida alpha y su respectivo plot

--- R/Indices de diversidad.R
+++ R/Indices de diversidad.R
@@ -17,17 +17,120 @@ stopifnot(!any(is.na(Diversidad_fichas)))
 ### Indices de diversidad alpha ###
 ###################################
 
+#########################
 ### Indice de Shannon ###
+#########################
 
-Diversidad <- Diversidad_fichas %>% column_to_rownames("Sitio") 
+Shannon <- function(abun){
+  total <- sum(abun)
+  frec <- abun/total
+  mult <- (log(frec))*frec
+  s<- -(sum(mult))
+  return(s)
+}
 
-alfa <- data.frame( 
-  sitio    = rownames(Diversidad), 
-  riqueza  = specnumber(Diversidad), 
-  shannon  = diversity(Diversidad, index = "shannon"), 
-  simpson  = diversity(Diversidad, index = "simpson"), 
-  inv_simp = diversity(Diversidad, index = "invsimpson"), 
-  pielou   = diversity(Diversidad, "shannon") / log(specnumber(sp_matrix)) 
-)
 
-lslk
+### Tabla con Sitio, Abundancia total y Shannon ###
+
+tabla_shannon <- Diversidad_fichas %>% # Se crea un objeto, donde se guarara la tabla, se asigna el data frame, y %>% toma la base de datos y aplica el siguiente argumento
+  group_by(Sitio) %>% # aqui se agrupa por el sitio. y se le aplica los filtros que siguen
+  summarise( # crea el resumen 
+    Abundancia_total = sum(Abundancias), # suma las abundanciass de cadda muestreo del sitio
+    shannon = Shannon(Abundancias) # Aplica el indice de shannon con la funcion creada en clase 
+  )
+
+print(tabla_shannon) # se visualiza la tabla
+
+#########################
+### Indice de Simpson ###
+#########################
+
+simpsonSimplificado <- function(ab){
+  tot <- sum(ab)
+  cuadrados <- (ab/tot)^2
+  adecion <- sum(cuadrados)
+  return (adecion)
+}
+
+tabla_simpson <- Diversidad_fichas %>% # Se crea un objeto, donde se guarara la tabla, se asigna el data frame, y %>% toma la base de datos y aplica el siguiente argumento
+  group_by(Sitio) %>% # aqui se agrupa por el sitio. y se le aplica los filtros que siguen
+  summarise( # crea el resumen 
+    Abundancia_total = sum(Abundancias), # suma las abundanciass de cadda muestreo del sitio
+    simpson = simpsonSimplificado(Abundancias) # Aplica el indice de shannon con la funcion creada en clase 
+  )
+
+tabla_simpson
+
+#################################
+### Indice de Simpson inverso ###
+#################################
+
+inverSimpson <- function (abun){
+  return(1/simpsonSimplificado(abun))
+}
+
+tabla_insimpson <- Diversidad_fichas %>% # Se crea un objeto, donde se guarara la tabla, se asigna el data frame, y %>% toma la base de datos y aplica el siguiente argumento
+  group_by(Sitio) %>% # aqui se agrupa por el sitio. y se le aplica los filtros que siguen
+  summarise( # crea el resumen 
+    Abundancia_total = sum(Abundancias), # suma las abundanciass de cadda muestreo del sitio
+    Simpson_inverso = inverSimpson(Abundancias) # Aplica el indice de shannon con la funcion creada en clase 
+  )
+
+tabla_insimpson
+
+########################
+### Indice de Pielou ###
+########################
+
+pielou <- function(abundancias){
+  S <- sum(abundancias > 0)
+  J <- Shannon(abundancias)/log(S)
+  return (J)
+}
+
+Diversidad_fichas$Muestra[Diversidad_fichas$Muestra == "morado"] <- 1
+Diversidad_fichas$Muestra[Diversidad_fichas$Muestra == "azul_turquesa"] <- 2
+Diversidad_fichas$Muestra[Diversidad_fichas$Muestra == "azul_cielo"] <- 3
+Diversidad_fichas$Muestra[Diversidad_fichas$Muestra == "azul_marino"] <- 4
+Diversidad_fichas$Muestra[Diversidad_fichas$Muestra == "fucsia"] <- 5
+Diversidad_fichas$Muestra[Diversidad_fichas$Muestra == "rosa_medio"] <- 6
+Diversidad_fichas$Muestra[Diversidad_fichas$Muestra == "rosa_claro"] <- 7
+Diversidad_fichas$Muestra[Diversidad_fichas$Muestra == "rojo"] <- 8
+Diversidad_fichas$Muestra[Diversidad_fichas$Muestra == "verde_limon"] <- 9
+Diversidad_fichas$Muestra[Diversidad_fichas$Muestra == "verde"] <- 10
+Diversidad_fichas$Muestra[Diversidad_fichas$Muestra == "naranja"] <- 11
+Diversidad_fichas$Muestra[Diversidad_fichas$Muestra == "amarillo"] <- 12
+
+Diversidad_fichas$Muestra <- as.numeric(Diversidad_fichas$Muestra)
+
+tabla_pielou <- Diversidad_fichas %>% # Se crea un objeto, donde se guarara la tabla, se asigna el data frame, y %>% toma la base de datos y aplica el siguiente argumento
+  group_by(Sitio) %>% # aqui se agrupa por el sitio. y se le aplica los filtros que siguen
+  summarise( # crea el resumen 
+    Abundancia_total = sum(Abundancias), # suma las abundanciass de cadda muestreo del sitio
+    Pielou = pielou(Abundancias) # Aplica el indice de shannon con la funcion creada en clase 
+  )
+
+tabla_pielou
+
+indices_alpha <- tabla_pielou %>%
+  left_join(tabla_insimpson, by = "Sitio") %>%
+  left_join(tabla_shannon, by = "Sitio") %>%
+  left_join(tabla_simpson, by = "Sitio")
+
+indices_alpha
+
+library(ggplot2)
+install.packages("gridExtra")
+library(gridExtra)
+
+## Crear la tabla ##
+tabla <- tableGrob(indices_alpha) # Transforma los objetos que forman la tabla en un plot (imagen)
+
+## Crear un gráfico vacío y añadir la tabla ##
+indices <- ggplot() +
+  annotation_custom(tabla)
+
+## Guardar el plot en resultados ##
+
+# Supongamos que tu objeto gráfico se llama indices
+ggsave("Figuras/idices_alpha.jpg", plot = indices, width = 8, height = 6)
